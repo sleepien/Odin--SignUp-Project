4a09b6929bf991d4b6956dcff86caf21d9c1af91
sleepien <108067513+sleepien@users.noreply.github.com>
2023-07-12 18:08:47 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import bg from "./assets/images/bg.avif";
+import logo from "./assets/images/odin-lined.png";
+
+function App() {
+  return (
+    <div className="bg-[url('./assets/images/bg.avif')] h-screen w-1/3 object-cover">
+      dfg
+    </div>
+  );
+}
+
+export default App;
+
+{
+  /* <div className="relative h-screen w-1/3">
+      <img src={bg} alt="bg" className="object-cover h-screen" />
+      <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-200%] flex justify-center items-center bg-[rgba(0,0,0,0.6)] w-full h-32">
+        <img src={logo} alt="logo" className="w-20 object-contain mr-2" />
+        <h1 className="font-bold text-5xl text-white">ODIN</h1>
+      </div>
+    </div> */
+}
